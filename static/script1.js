21a8ae716066a6a84083f6c77d8bd5454054bd2b
Tireni Adekoya <tirenioluwa.adek@bulldogs.aamu.edu>
2024-04-07 11:42:05 -0500

    Update began web app using flask and now scripting event listeners and handlers

--- static/script1.js
+++ static/script1.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Dom is loaded")
+
+
     let cells = document.querySelectorAll('.cell');
     cells.forEach(function(cell) {
         cell.addEventListener('click', function(){
+            cell.classList.add('x');
             console.log("Clicked");
         });
     });
